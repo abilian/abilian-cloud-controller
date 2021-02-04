f5459c6006e32c1824140c1868b3c58d7b358486
Stefane Fermigier <sf@fermigier.com>
2021-02-04 18:40:05 +0100

    Tests / CI.

---
+++ tests/test_main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from acc.config import PORT
+from acc.commands import *
+from acc.processes import supervisor
+
+from unittest import TestCase
+import urllib
+import time
+
+
+class FunctionalTestCase(TestCase):
+
+  @classmethod
+  def setUpClass(cls):
+    cmd_clean()
+    cmd_boot()
+
+  @classmethod
+  def tearDownClass(cls):
+    cmd_halt()
+    cmd_clean()
+
+  def test(self):
+    instances = cmd_list()
+    instance_num = len(instances)
+
+    instance = cmd_create()
+    iid = instance.iid
+
+    instances = cmd_list()
+    self.assertEqual(instance_num+1, len(instances))
+
+    time.sleep(10)
+    urllib.urlopen("http://localhost:%d/nuxeo/" % instance.port).read()
+    urllib.urlopen("http://%s:%d/nuxeo/" % (instance.hostname, PORT)).read()
+
+    cmd_stop(iid)
+    cmd_destroy(iid)
+    cmd_purge(iid)
+
+
+class SupervisorTestCase(TestCase):
+
+  def test(self):
+    supervisor.gen_conf()
+    supervisor.start()
+    time.sleep(5)
+    supervisor.stop()
